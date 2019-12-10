6208fddad632e9d7148c7cefd1659fe78a8d31b9
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-12-10 16:47:27 +0300

    Consumer app was added

---
+++ RabbitMQ.OrderProcessor/Program.cs
@@ -0,0 +1,17 @@
+﻿using EasyNetQ;
+using RabbitMQApi.Controllers;
+using System;
+
+namespace RabbitMQ.OrderProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messageBus = RabbitHutch.CreateBus("host=localhost");
+            messageBus.Subscribe<CarOrder>("app_subscription_id", msg=> {
+                Console.WriteLine("Processing order: {0} -- Model: {1} -- Color: {2}", msg.CustomerEmail, msg.Model, msg.Color);
+            });
+        }
+    }
+}
