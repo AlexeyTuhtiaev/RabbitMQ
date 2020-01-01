f61d4e2684a3d51507be430dc1dacceff5cb1a27
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2020-01-01 19:37:48 +0300

    Sending into default exchange

---
+++ Consumer1/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qName1",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" Received message: {0}", message);
+                };
+                channel.BasicConsume(queue: "qName1",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("Subscribed to the queue <qName1>");
+                Console.WriteLine("Listerning . . .");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Produser1/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Produser1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                int timeToSleep = new Random().Next(1000, 2500);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "qName1",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"The current time is {DateTime.Now}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "qName1",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine($" [{count++}] sent message into Default Exchange");
+                }
+            } while (true);
+        }
+    }
+}
