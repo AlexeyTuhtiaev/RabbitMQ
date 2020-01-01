b5d6ee8c4baf29f9c37a2a9b1e46230b55fdcde0
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2020-01-01 21:22:17 +0300

    2 - Work queues

---
+++ Consumer2/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //don't dispatch a new message to a worker until it has processed 
+                //and acknowledged the previous one
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    int timeToSleep =  new Random().Next(1000, 2000);
+                    Thread.Sleep(timeToSleep); //imitation of work 
+
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    // manually send a proper acknowledgment from the proceeding  process
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                    Console.WriteLine("Processed message: {0}", message);
+                };
+                channel.BasicConsume(queue: "task_queue",
+                                     autoAck: false,// manually send a proper acknowledgment from the proceeding  process
+                                     consumer: consumer);
+
+                Console.WriteLine("Subscribed to the queue <task_queue>");
+                Console.WriteLine("Listerning . . .");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Produser2/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Produser2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                int timeToSleep = 1000;
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"[{count}] The current time is {DateTime.Now}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "task_queue",
+                                         basicProperties: properties,
+                                         body: body);
+                    
+                    Console.WriteLine($"[{count++}] sent message into Default Exchange");
+                }
+            } while (true);
+        }
+    }
+}
