5ede0ab2785745328bc18eee0e904d64fc21631f
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2020-01-06 19:26:34 +0300

    Work with fanout

---
+++ Consumer6_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer6_2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "fanout_ex", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "fanout_ex",
+                                  routingKey: "*.*.*.ecological");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("Processed message: {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine($"Subscribed to the queue <{queueName}>");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer6/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Producer6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                int timeToSleep = new Random().Next(1000, 2000);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "fanout_ex", type: "fanout");
+
+                    string routingKey = count % 4 == 0
+                        ? "Tesla.red.fast.ecological" :
+                        count % 5 == 0 ? "Mersedes.executive.expensive.ecological" : "whatever";
+
+                    string message = $"[{count}] [{routingKey}] sent at {DateTime.Now}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "fanout_ex",
+                                         routingKey: routingKey,
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine($"[{count++}] sent message with [{routingKey}] into 'fanout_ex' Exchange");
+                }
+            } while (true);
+        }
+    }
+}
